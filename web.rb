9a74225811c7df4ff7c5973d62d65999a053805d
Nicholas Scheurich <nick@scheurich.me>
2016-03-23 15:51:49 -0500

    Serve content as application/json

--- web.rb
+++ web.rb
@@ -3,6 +3,8 @@ require "httparty"
 require "active_support/core_ext/hash"
 
 get "/" do
+  content_type :json
+
   base_uri = "https://braiservices.dticloud.com/cgi-bin/hm_hm.sh/interface/adplus/lib/interface"
   query = {
     ActionType: "RunProg",
@@ -11,8 +13,6 @@ get "/" do
   }
 
   response = HTTParty.get(base_uri, query: query)
-
   xml = response.body
-
   Hash.from_xml(xml).to_json
 end
